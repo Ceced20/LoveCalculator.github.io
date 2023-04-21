da055a7f372582bb6559877be17c9be4ce2a9fb8
Ceced20 <122191670+Ceced20@users.noreply.github.com>
2023-04-21 14:46:25 +0700

    Removing Random to a algorithm

--- script.js
+++ script.js
@@ -14,27 +14,44 @@ calculateBtn.addEventListener("click", calculateLove);
 function calculateLove() {
     const name1 = girlNameInput.value.toLowerCase();
     const name2 = boyNameInput.value.toLowerCase();
-    let lovePercentage = 0;
 
     if (name1 === "" || name2 === "") {
         alert("Please enter both names.");
     } else {
+        // Convert names to arrays of letters
+        const letters1 = name1.split("");
+        const letters2 = name2.split("");
+
+        let count = 0;
+
+        // loop through each letter in name1
+        for (let i = 0; i < letters1.length; i++) {
+            // loop through each letter in name2
+            for (let j = 0; j < letters2.length; j++) {
+                // if letters match, increment count and remove from letters2 to avoid duplicates
+                if (letters1[i] === letters2[j]) {
+                    count++;
+                    letters2.splice(j, 1);
+                    break;
+                }
+            }
+        }
+
         // Calculate the love percentage
-        lovePercentage = Math.floor(Math.random() * 100) + 1;
+        const lovePercentage = Math.floor((count / name1.length) * 100);
 
         // Special cases
         if (name1 === "mpok jowo" && name2 === "ronaldo ruzali") {
-            lovePercentage = 100;
-        }
-        if (name1 === "angel" && name2 === "brandon") {
-            lovePercentage = 100;
-        }
-
-        // Display the result
-        if (lovePercentage === 0) {
-            resultEl.innerHTML = `Sorry, ${name1} and ${name2} are not compatible.`;
+            resultEl.innerHTML = "100% Compatible!";
+        } else if (name1 === "angel" && name2 === "brandon") {
+            resultEl.innerHTML = "100% Compatible!";
         } else {
-            resultEl.innerHTML = `${name1} and ${name2} are ${lovePercentage}% compatible!`;
+            // Display the result
+            if (lovePercentage === 0) {
+                resultEl.innerHTML = `Sorry, ${name1} and ${name2} are not compatible, ${name1} and ${name2} are ${lovePercentage}%`;
+            } else {
+                resultEl.innerHTML = `${name1} and ${name2} are ${lovePercentage}% compatible!`;
+            }
         }
     }
 }
